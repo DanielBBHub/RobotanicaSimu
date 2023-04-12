4f2a4ea8b48cf7a27068c8a5f9cb4ced9c2566ab
rubenM <rubenmarinsamper@gmail.com>
2023-04-12 11:24:09 +0200

    Commit lidar

--- Assets/Scripts/ROS2PublisherExample.cs
+++ Assets/Scripts/ROS2PublisherExample.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.UnityRoboticsDemo;
 
@@ -52,4 +52,58 @@ public class ROS2PublisherExample : MonoBehaviour
         }
     }
 }
+*/
 
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.UnityRoboticsDemo;
+
+/// <summary>
+///
+/// </summary>
+public class ROS2PublisherExample : MonoBehaviour
+{
+    ROSConnection ros;
+    public string topicName = "pos_rot";
+
+    // El Nombre del game object
+    public GameObject cube;
+ 
+    // Publica la posición y rotación del cubo cada N segundos
+    public float publishMessageFrequency = 0.5f;
+
+    // Permite determinar el tiempo transcurrido desde la publicación del último mensaje.
+    private float timeElapsed;
+
+    void Start()
+    {
+        // Iniciar la conexión ROS
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<PosRotMsg>(topicName);
+    }
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > publishMessageFrequency)
+        {
+            cube.transform.rotation = Random.rotation;
+
+            PosRotMsg cubePos = new PosRotMsg(
+                cube.transform.position.x,
+                cube.transform.position.y,
+                cube.transform.position.z,
+                cube.transform.rotation.x,
+                cube.transform.rotation.y,
+                cube.transform.rotation.z,
+                cube.transform.rotation.w
+            );
+
+            // Finalmente envía el mensaje a server_endpoint.py que se ejecuta en ROS
+            ros.Publish(topicName, cubePos);
+
+            timeElapsed = 0;
+        }
+    }
+}
